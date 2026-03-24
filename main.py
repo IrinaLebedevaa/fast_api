a0e81399f03c37bab8a23874f31e6dc8023aba8a
Irina <147788502+IrinaLebedevaa@users.noreply.github.com>
2026-03-24 19:21:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,49 +13,7 @@ templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/hello")  # декоратор определяет путь, запросы по которому будет обрабатывать ф
-def read_root():  #функция, которая обрабатывает запросы
-    html_content = "<h2>Hello VODOLEY!</h2>"
-    return HTMLResponse(content=html_content)
 
-
-@app.get("/about")  #http://127.0.0.1:8000/about
-def about():
-    html_content = "<h3>About Vodoley????</h3>"
-    return HTMLResponse(content=html_content)  #content задает отправляемые данные
-
-
-@app.get("/text", response_class = PlainTextResponse)
-def root_text():
-    return "Vodoley PlainTextResponse beee"
-
-
-@app.get("/eee")
-def root_html():
-    return FileResponse("templates/index.html", media_type="text/html")
-
-"""
-@app.get("/users/{name}/{alone}/{stage_fear}/{social_event}/{going}/{grained}/{friends}/{post}")
-def users(name: str,
-          alone: int,
-          stage_fear: int,
-          social_event: int,
-          going: int,
-          grained: int,
-          friends: int,
-          post: int,
-          request: Request
-          ):
-    temp = model.personality(alone, stage_fear, social_event, going, grained, friends, post)
-    result = temp.to_dict()[0]
-    return templates.TemplateResponse("index.html", {
-        "request": request,      # обязательно
-        "name": name,
-        "prediction": result
-    })
-    #return HTMLResponse(f'{name} - вы {temp.to_dict()[0]}')
-
-"""
 @app.get("/", response_class=HTMLResponse)
 def form_page(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
